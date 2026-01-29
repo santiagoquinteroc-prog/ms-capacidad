fdb3050b3cbbeedfb6cd9dac97e4db4c85bd5740
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-01-29 07:30:43 -0500

    chore: init ms-capacidad spring initializr

---
+++ src/main/java/com/reto/ms_capacidad/MsCapacidadApplication.java
@@ -0,0 +1,13 @@
+package com.reto.ms_capacidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCapacidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCapacidadApplication.class, args);
+	}
+
+}
